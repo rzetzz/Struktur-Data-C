0e0cfb6f974f42923b50a9bc37bebf85fda63eec
Fatkhur Rozi <30801605+rzetzz@users.noreply.github.com>
2023-05-09 21:32:30 +0700

    Add files via upload

---
+++ LatihanModul4/src/Mahasiswa.java
@@ -0,0 +1,34 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Mahasiswa {
+    String nama, kelas, matkulPraktikum;
+    int nim;
+
+    public Mahasiswa(String nm, String kl, String mat, int ni){
+        nama = nm;
+        kelas = kl;
+        matkulPraktikum = mat;
+        nim  = ni;
+    }
+
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        HashMap<String, Mahasiswa> mhs = new HashMap<>();
+        String input;
+        Mahasiswa data;
+
+
+        mhs.put("1", new Mahasiswa("Putri", "3H", "struktur data", 20200000));
+        mhs.put("2", new Mahasiswa("Agus", "3A", "Diskrit", 20200089));
+        mhs.put("3", new Mahasiswa("Arro", "3c", "basis data", 20200066));
+
+        System.out.println("Masukan ID : ");
+        input= in.nextLine();
+        data = mhs.get(input);
+        if(data != null){
+            System.out.println("Data Mahasiswa : "+ data.nama + ", kelas : "+ data.kelas +
+                    ", mata kuliah praktikum : " +  data.matkulPraktikum + ", nim :" + data.nim);
+        }
+    }
+}
